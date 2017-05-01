6a5964b0bec8d810ab124372e3c9e1382763b34b
Dave Payne <david.payne@multicom.co.uk>
2017-05-01 21:58:06 +0100

    Remove unused modifier methods on entity classes

--- src/main/java/com/cyanelix/railwatch/entity/ScheduleEntity.java
+++ src/main/java/com/cyanelix/railwatch/entity/ScheduleEntity.java
@@ -32,51 +32,23 @@ public class ScheduleEntity {
                 schedule.getNotificationTarget().getTargetAddress());
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public LocalTime getStartTime() {
         return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
     }
 
     public LocalTime getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
     public String getFromStation() {
         return fromStation;
-    }
-
-    public void setFromStation(String fromStation) {
-        this.fromStation = fromStation;
     }
 
     public String getToStation() {
         return toStation;
     }
 
-    public void setToStation(String toStation) {
-        this.toStation = toStation;
-    }
-
     public String getNotificationTarget() {
         return notificationTarget;
-    }
-
-    public void setNotificationTarget(String notificationTarget) {
-        this.notificationTarget = notificationTarget;
     }
 }
--- src/main/java/com/cyanelix/railwatch/entity/SentNotificationEntity.java
+++ src/main/java/com/cyanelix/railwatch/entity/SentNotificationEntity.java
@@ -32,14 +32,6 @@ public class SentNotificationEntity {
     private String priority;
     private LocalDateTime sentDateTime;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getTo() {
         return to;
     }
@@ -52,31 +44,7 @@ public class SentNotificationEntity {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public LocalDateTime getSentDateTime() {
-        return sentDateTime;
-    }
-
-    public void setSentDateTime(LocalDateTime sentDateTime) {
-        this.sentDateTime = sentDateTime;
     }
 }
