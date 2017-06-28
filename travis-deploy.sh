50d12ca0fb5039154075d695dad4bff64c791d32
Dave Payne <david.payne@multicom.co.uk>
2017-06-28 06:45:22 +0100

    Revert "Move docker push to a script for Travis"
    This reverts commit a4c87679f3ea8b81d938bcc4c23efc09e523a291.

--- travis-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-cat "Branch: $TRAVIS_BRANCH"
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    docker push USER/REPO;
-fi
