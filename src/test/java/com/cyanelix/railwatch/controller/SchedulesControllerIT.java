0832b06b2c29559aed78a5f10238fdd055250c18
Dave Payne <david.payne@multicom.co.uk>
2017-10-31 16:53:14 +0000

    Enhance Schedules integration test
    - Check that a requested schedule is actually added and returned in a
      subsequent GET.

--- src/test/java/com/cyanelix/railwatch/controller/SchedulesControllerIT.java
+++ src/test/java/com/cyanelix/railwatch/controller/SchedulesControllerIT.java
@@ -5,11 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -22,7 +28,39 @@ public class SchedulesControllerIT {
 
     @Test
     public void createSchedule_listAllSchedules_scheduleIsReturned() throws Exception {
+        String uuid = UUID.randomUUID().toString();
+
+        mockMvc.perform(
+                put("/schedules")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildRequest(uuid)))
+                .andExpect(status().isCreated());
+
         mockMvc.perform(get("/schedules"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResponse(uuid)));
+    }
+
+    private String buildRequest(String uuid) {
+        return "{\n" +
+                "  \"startTime\": \"10:00\",\n" +
+                "  \"endTime\": \"11:00\",\n" +
+                "  \"days\": [\"Monday\"],\n" +
+                "  \"fromStation\": \"PAD\",\n" +
+                "  \"toStation\": \"BRI\",\n" +
+                "  \"notificationTarget\": \"" + uuid + "\"\n" +
+                "}";
+    }
+
+    private String expectedResponse(String uuid) {
+        return "[{\n" +
+                "  \"startTime\": \"10:00\",\n" +
+                "  \"endTime\": \"11:00\",\n" +
+                "  \"days\": [\"Monday\"],\n" +
+                "  \"fromStation\": \"PAD\",\n" +
+                "  \"toStation\": \"BRI\",\n" +
+                "  \"notificationTarget\": \"" + uuid + "\"\n" +
+                "}]";
     }
 }
