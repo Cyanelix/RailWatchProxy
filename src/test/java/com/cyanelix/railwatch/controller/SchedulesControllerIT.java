94efb029ece61eba6601e28d89e7ca396ba612af
Dave Payne <david.payne@multicom.co.uk>
2017-11-01 06:49:29 +0000

    Add tests for enabled/disabled state of schedules being created
    - Also adds a temporary test with a TODO that can be removed as soon as
      the version number is incremented to 3 or higher, and the requirement
      for state to be optional is removed.

--- src/test/java/com/cyanelix/railwatch/controller/SchedulesControllerIT.java
+++ src/test/java/com/cyanelix/railwatch/controller/SchedulesControllerIT.java
@@ -1,5 +1,8 @@
 package com.cyanelix.railwatch.controller;
 
+import com.cyanelix.railwatch.domain.ScheduleState;
+import com.cyanelix.railwatch.repository.ScheduleRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.UUID;
 
@@ -26,41 +28,91 @@ public class SchedulesControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Before
+    public void setup() {
+        scheduleRepository.deleteAll();
+    }
+
     @Test
-    public void createSchedule_listAllSchedules_scheduleIsReturned() throws Exception {
+    public void createScheduleWithoutState_listAllSchedules_scheduleIsReturnedAsEnabled() throws Exception {
+        // TODO: From v3.x null states no longer needs supporting as state will be required, so this test can be removed.
         String uuid = UUID.randomUUID().toString();
 
         mockMvc.perform(
                 put("/schedules")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(buildRequest(uuid)))
+                        .content(buildRequest(uuid, null)))
                 .andExpect(status().isCreated());
 
         mockMvc.perform(get("/schedules"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(expectedResponse(uuid)));
+                .andExpect(content().json(expectedResponse(uuid, ScheduleState.ENABLED)));
     }
 
-    private String buildRequest(String uuid) {
+    @Test
+    public void createEnabledSchedule_listAllSchedules_scheduleIsReturnedAsEnabled() throws Exception {
+        String uuid = UUID.randomUUID().toString();
+
+        mockMvc.perform(
+                put("/schedules")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildRequest(uuid, ScheduleState.ENABLED)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/schedules"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResponse(uuid, ScheduleState.ENABLED)));
+    }
+
+    @Test
+    public void createDisabledSchedule_listAllSchedules_scheduleIsReturnedAsDisabled() throws Exception {
+        String uuid = UUID.randomUUID().toString();
+
+        mockMvc.perform(
+                put("/schedules")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildRequest(uuid, ScheduleState.DISABLED)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/schedules"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedResponse(uuid, ScheduleState.DISABLED)));
+    }
+
+    private String buildRequest(String uuid, ScheduleState scheduleState) {
+        String state = "";
+
+        if (scheduleState != null) {
+            state = "  \"state\": \"" + scheduleState.name() + "\",\n";
+        }
+
         return "{\n" +
                 "  \"startTime\": \"10:00\",\n" +
                 "  \"endTime\": \"11:00\",\n" +
                 "  \"days\": [\"Monday\"],\n" +
                 "  \"fromStation\": \"PAD\",\n" +
                 "  \"toStation\": \"BRI\",\n" +
+                state +
                 "  \"notificationTarget\": \"" + uuid + "\"\n" +
                 "}";
     }
 
-    private String expectedResponse(String uuid) {
+    private String expectedResponse(String uuid, ScheduleState scheduleState) {
         return "[{\n" +
                 "  \"startTime\": \"10:00\",\n" +
                 "  \"endTime\": \"11:00\",\n" +
                 "  \"days\": [\"Monday\"],\n" +
                 "  \"fromStation\": \"PAD\",\n" +
                 "  \"toStation\": \"BRI\",\n" +
-                "  \"notificationTarget\": \"" + uuid + "\"\n" +
+                "  \"notificationTarget\": \"" + uuid + "\",\n" +
+                "  \"state\": \"" + scheduleState.name() + "\"\n" +
                 "}]";
     }
+
 }
